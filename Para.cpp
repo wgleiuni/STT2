56da5c8b86c805b1a4f9bf0fa3e39f9d3f76ca50
Guanglei Wang <wgleiuni@gmail.com>
2017-02-24 11:24:04 -0700

    Update
    Update the parameter outputs

--- Para.cpp
+++ Para.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <vector>
 #include <string>
+#include <iomanip>
 #include "Para.h"
 using namespace std;
 // Allocating and initializing static data member
@@ -43,12 +44,25 @@ void Parameter::read() {
         istringstream iss(line);
         iss>>_y[0]>>_y[1]>>_y[2];
     }
-    cout << "alpha = " << _alpha << endl;
-    cout << "kappa = " << _kappa << endl;
-    cout << "mu = " << _mu << endl;
-    cout << "nu = " << _nu << endl;
-    cout << "v = " << _v[0] << endl;
-    cout << "dt = " << _h << endl;
-    cout << "m0 = " << _y[0] << " " << _y[1] << " " << _y[2] << endl;
+    cout.precision(6);
+    cout << string(35,'-') << "Parameter" << string(35,'-') << endl;
+    cout << setw(16) << "\u03B1 = "+to_string(_alpha)+"  ";
+    cout << setw(16) << "\u03BA = "+to_string(_kappa)+"  ";
+    cout << setw(16) << "\u03BC = "+to_string(_mu)+"  ";
+    cout << setw(16) << "\u03BD = "+to_string(_nu)+"  ";
+    cout << setw(16) << "v = "+to_string(_v[0])+"  " << endl;
+    cout << "Important component: " << string(1,'x'+_o) << endl;
+    cout << "Initial conditions: ";
+    cout << setw(16) << "m1 = "+to_string(_y[0])+"  ";
+    cout << setw(16) << "m2 = "+to_string(_y[1])+"  ";
+    cout << setw(16) << "m3 = "+to_string(_y[2])+"  " << endl;
+    cout << "External field: ";
+    cout << setw(16) << "b1 = "+to_string(_b[0])+"  ";
+    cout << setw(16) << "b2 = "+to_string(_b[1])+"  ";
+    cout << setw(16) << "b3 = "+to_string(_b[2])+"  " << endl;
+    cout << string(34,'-') << "Simulation" << string(35,'-') << endl;
+    cout << setw(16) << "dt = "+to_string(_h)+"  ";
+    cout << setw(16) << "Total Time = "+to_string(_totT)+"  ";
+    cout << setw(16) << "Record Step = "+to_string(_numdt)+"  " << endl;
     return;
 }
